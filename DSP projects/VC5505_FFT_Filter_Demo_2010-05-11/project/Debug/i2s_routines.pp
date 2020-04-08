f5c1ed755de865d84d03f3fc66f5f5ee39ea51ba
Hacker Gnome <47784903+Rejdler@users.noreply.github.com>
2020-04-08 14:47:15 +0200

    I give up

---
+++ DSP projects/VC5505_FFT_Filter_Demo_2010-05-11/project/Debug/i2s_routines.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+i2s_routines.obj: ../i2s_routines.c
+i2s_routines.obj: ../data_types.h
+i2s_routines.obj: ../register_cpu.h
+i2s_routines.obj: ../i2s_routines.h
+
+../i2s_routines.c: 
+../data_types.h: 
+../register_cpu.h: 
+../i2s_routines.h: 
