d4d9b9965d708c633f7064e10d53bb50b6335c9b
Holger Bovbjerg <holger.bovbjerg@hotmail.com>
2020-04-16 15:03:01 +0200

    Revert "Filter_blueprint"
    This reverts commit 035bac898bb4885aabf132e8b66dc6658dccdf4e.

--- Filter_skabelon/source_filer/Debug/configuration.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-configuration.obj: ../configuration.c
-configuration.obj: ../data_types.h
-configuration.obj: ../configuration.h
-
-../configuration.c: 
-../data_types.h: 
-../configuration.h: 
--- Filter_skabelon/source_filer/Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-LIBS := $(GEN_CMDS_QUOTED) -l"rts55h.lib" -l"C:\Users\dksan\Desktop\Filter_skabelon\source_filer\lib\55xdsph_r3.lib"
-
-USER_OBJS :=
