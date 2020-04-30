bf804747075ffbc6a10b14c58430e87636e71330
Holger Bovbjerg <holger.bovbjerg@hotmail.com>
2020-05-01 00:00:10 +0200

    Add code for sampling codec data and write to file
    Needs to be tested on DSP

---
+++ DSP projects/codec_sampler/sar.h
@@ -0,0 +1,14 @@
+#define SARCTRL      ((ioport volatile unsigned*)0x7012)
+#define SARDATA      ((ioport volatile unsigned*)0x7014)
+#define SARCLKCTRL   ((ioport volatile unsigned*)0x7016)
+#define SARPINCTRL   ((ioport volatile unsigned*)0x7018)
+#define SARGPOCTRL   ((ioport volatile unsigned*)0x701A)
+
+#define SW1   0x2A8  // Switch 1 value
+#define SW2   0x1FC  // Switch 2 value
+#define SW12  0x195  // Switch 1 + 2 value
+#define NoKey 0x3FE
+
+/* Prototype functions */
+void Init_SAR(void);
+Uint16 Get_Sar_Key(void);
